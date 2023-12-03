8df2fedb69e08a978b2ffdfd7cc3fc0dbf5fe9c8
Rob Thomas <robt84@gmail.com>
2023-12-03 10:13:56 -0600

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
-import { Inter } from 'next/font/google'
+import { Inter, Asap, Poppins } from 'next/font/google'
 import './globals.css'
+import mainHeader from '../app/components/headers/mainHeader'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <mainHeader />
+        {children}
+        </body>
     </html>
   )
 }
