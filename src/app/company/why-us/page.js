f18c61b2e581153f350117c5475c454abbc0a038
Rob Thomas <robt84@gmail.com>
2023-12-09 13:44:12 -0600

    add components to the why us page

--- src/app/company/why-us/page.js
+++ src/app/company/why-us/page.js
@@ -1,5 +1,21 @@
+import ClientSuccessStories from "@/app/components/global/ClientSuccessStories";
+import SimpleCTA from "@/app/components/global/SimpleCTA";
+import IndustryRecognition from "@/app/components/why-us/IndustryRecognition";
+import InnovativeSolutions from "@/app/components/why-us/InnovativeSolutions";
+import OurExpertise from "@/app/components/why-us/OurExpertise";
+import OurStatus from "@/app/components/why-us/OurStatus";
+import ReliableSupport from "@/app/components/why-us/ReliableSupport";
+import WhyHero from "@/app/components/why-us/WhyHero";
+
 export default function Page() {
     return <main>
-        <h1>Hello, Why Us Page!</h1>
+        <WhyHero />
+        <OurStatus />
+        <OurExpertise />
+        <InnovativeSolutions />
+        <ClientSuccessStories />
+        <ReliableSupport />
+        <IndustryRecognition />
+        <SimpleCTA />
     </main>
   }
