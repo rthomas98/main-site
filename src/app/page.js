31676520354d4fa406e5249c16781a9fff7743de
Rob Thomas <robt84@gmail.com>
2023-12-09 10:48:31 -0600

    added components to the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+import DarkCTA from "./components/global/DarkCTA"
+import LogoCloud from "./components/global/LogoCloud"
+import PurpleTestimonials from "./components/global/PurpleTestimonials"
+import RecentCaseStudies from "./components/global/RecentCaseStudies"
+import RecentPosts from "./components/global/RecentPosts"
+import HomeAboutUs from "./components/home/HomeAboutUs"
+import HomeSolutions from "./components/home/HomeSolutions"
+import HomeWhyUs from "./components/home/HomeWhyUs"
 import HomeHero from "./components/home/homeHero"
 
 export default function Home() {
   return (
     <main>
       <HomeHero />
+      <LogoCloud />
+      <HomeAboutUs />
+      <HomeSolutions />
+      <RecentCaseStudies />
+      <PurpleTestimonials />
+      <HomeWhyUs />
+      <DarkCTA />
+      <RecentPosts />
     </main>
   )
 }
