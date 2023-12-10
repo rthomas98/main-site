312d855c7c5f7806a552dbe9e5f7368926d0bfe2
Rob Thomas <robt84@gmail.com>
2023-12-09 19:01:20 -0600

    added new components to the approach page

--- src/app/company/approach/page.js
+++ src/app/company/approach/page.js
@@ -1,4 +1,23 @@
+import DarkCTA from "@/app/components/global/DarkCTA";
+import ApproachHeader from "@/app/components/our-approach/ApproachHeader";
+import ClientCollaboration from "@/app/components/our-approach/ClientCollaboration";
+import ContinuousImprovement from "@/app/components/our-approach/ContinuousImprovement";
+import DeploymentLaunch from "@/app/components/our-approach/DeploymentLaunch";
+import OngoingSupportMaintenance from "@/app/components/our-approach/OngoingSupportMaintenance";
+import QualityAssurance from "@/app/components/our-approach/QualityAssurance";
+import StrategicPlanning from "@/app/components/our-approach/StrategicPlanning";
+import UnderstandingYourNeeds from "@/app/components/our-approach/UnderstandingYourNeeds";
 
 export default function Page() {
-    return <h1>Hello, Approach Page!</h1>
+    return <main>
+      <ApproachHeader />
+      <UnderstandingYourNeeds />
+      <StrategicPlanning />
+      <QualityAssurance />
+      <DeploymentLaunch />
+      <OngoingSupportMaintenance />
+      <ContinuousImprovement />
+      <ClientCollaboration />
+      <DarkCTA />
+    </main>
   }
