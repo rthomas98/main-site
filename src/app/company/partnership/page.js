75c2e0c8ef3ea5020dd3f6d4667ad5e881a687ed
Rob Thomas <robt84@gmail.com>
2023-12-11 07:58:33 -0600

    Add in more components

--- src/app/company/partnership/page.js
+++ src/app/company/partnership/page.js
@@ -1,5 +1,21 @@
+import LogoCloud from "@/app/components/global/LogoCloud";
+import PurpleTestimonials from "@/app/components/global/PurpleTestimonials";
+import HowWeSupportOurPartners from "@/app/components/partnership/HowWeSupportOurPartners";
+import PartnershipFAQs from "@/app/components/partnership/PartnershipFAQs";
+import PartnershipForm from "@/app/components/partnership/PartnershipForm";
+import TypesOfPartnerships from "@/app/components/partnership/TypesOfPartnerships";
+import WhyPartner from "@/app/components/partnership/WhyPartner";
+import PartnershipHero from "@/app/components/partnership/partnershipHero";
+
 export default function Page() {
-    return <div className="container">
-        <h1>Partnership</h1>
-    </div>
+    return <main>
+        <PartnershipHero />
+        <LogoCloud />
+        <WhyPartner />
+        <TypesOfPartnerships />
+        <PurpleTestimonials />
+        <HowWeSupportOurPartners />
+        <PartnershipFAQs />
+        <PartnershipForm />
+    </main>
   }
