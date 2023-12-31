4b93dd963bb01c5927f1fb651651ca7c709335f8
Rob Thomas <robt84@gmail.com>
2023-12-30 23:00:24 -0600

    add the content and image for the home about us section

--- src/app/components/home/HomeSolutions.js
+++ src/app/components/home/HomeSolutions.js
@@ -1,70 +1,119 @@
-'use client'
+"use client";
 
- import React from 'react'
- import { InboxIcon, TrashIcon, UsersIcon } from '@heroicons/react/24/outline'
+import React from "react";
+import { InboxIcon, TrashIcon, UsersIcon } from "@heroicons/react/24/outline";
 
- const features = [
-    {
-      name: 'Unlimited inboxes',
-      description:
-        'Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.',
-      href: '#',
-      icon: InboxIcon,
-    },
-    {
-      name: 'Manage team members',
-      description:
-        'Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.',
-      href: '#',
-      icon: UsersIcon,
-    },
-    {
-      name: 'Spam report',
-      description:
-        'Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.',
-      href: '#',
-      icon: TrashIcon,
-    },
-  ]
- 
- function HomeSolutions() {
-   return (
+const features = [
+  {
+    name: "Unlimited inboxes",
+    description:
+      "Non quo aperiam repellendus quas est est. Eos aut dolore aut ut sit nesciunt. Ex tempora quia. Sit nobis consequatur dolores incidunt.",
+    href: "#",
+    icon: InboxIcon,
+  },
+  {
+    name: "Manage team members",
+    description:
+      "Vero eum voluptatem aliquid nostrum voluptatem. Vitae esse natus. Earum nihil deserunt eos quasi cupiditate. A inventore et molestiae natus.",
+    href: "#",
+    icon: UsersIcon,
+  },
+  {
+    name: "Spam report",
+    description:
+      "Et quod quaerat dolorem quaerat architecto aliquam accusantium. Ex adipisci et doloremque autem quia quam. Quis eos molestiae at iure impedit.",
+    href: "#",
+    icon: TrashIcon,
+  },
+];
+
+function HomeSolutions() {
+  return (
     <div className="bg-white py-24 sm:py-32">
-    <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <div className="mx-auto max-w-2xl lg:mx-0">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Stay on top of customer support
-        </h2>
-        <p className="mt-6 text-lg leading-8 text-gray-600">
-          Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-          accusamus quisquam.
-        </p>
-      </div>
-      <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
-        <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
-          {features.map((feature) => (
-            <div key={feature.name} className="flex flex-col">
-              <dt className="text-base font-semibold leading-7 text-gray-900">
-                <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
-                  <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+      <div className="relative isolate">
+        <svg
+          className="absolute inset-x-0 top-0 -z-10 h-[64rem] w-full stroke-gray-200 [mask-image:radial-gradient(32rem_32rem_at_center,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84"
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M.5 200V.5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <path
+              d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect
+            width="100%"
+            height="100%"
+            strokeWidth={0}
+            fill="url(#1f932ae7-37de-4c0a-a8b0-a6e3b4d44b84)"
+          />
+        </svg>
+        <div
+          className="absolute left-1/2 right-0 top-0 -z-10 -ml-24 transform-gpu overflow-hidden blur-3xl lg:ml-24 xl:ml-48"
+          aria-hidden="true"
+        >
+          <div
+            className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
+            style={{
+              clipPath:
+                "polygon(63.1% 29.5%, 100% 17.1%, 76.6% 3%, 48.4% 0%, 44.6% 4.7%, 54.5% 25.3%, 59.8% 49%, 55.2% 57.8%, 44.4% 57.2%, 27.8% 47.9%, 35.1% 81.5%, 0% 97.7%, 39.2% 100%, 35.2% 81.4%, 97.2% 52.8%, 63.1% 29.5%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Stay on top of customer support
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
+              magnam voluptatum cupiditate veritatis in accusamus quisquam.
+            </p>
+          </div>
+          <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-none">
+            <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-3">
+              {features.map((feature) => (
+                <div key={feature.name} className="flex flex-col">
+                  <dt className="text-base font-semibold leading-7 text-gray-900">
+                    <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <feature.icon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
+                    </div>
+                    {feature.name}
+                  </dt>
+                  <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
+                    <p className="flex-auto">{feature.description}</p>
+                    <p className="mt-6">
+                      <a
+                        href={feature.href}
+                        className="text-sm font-semibold leading-6 text-indigo-600"
+                      >
+                        Learn more <span aria-hidden="true">→</span>
+                      </a>
+                    </p>
+                  </dd>
                 </div>
-                {feature.name}
-              </dt>
-              <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-600">
-                <p className="flex-auto">{feature.description}</p>
-                <p className="mt-6">
-                  <a href={feature.href} className="text-sm font-semibold leading-6 text-indigo-600">
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
-                </p>
-              </dd>
-            </div>
-          ))}
-        </dl>
+              ))}
+            </dl>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-   )
- }
- 
- export default HomeSolutions
+  );
+}
+
+export default HomeSolutions;
