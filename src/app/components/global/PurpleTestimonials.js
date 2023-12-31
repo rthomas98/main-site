30c0b801d542e8d2d649b027fe070ed4c61b3381
Rob Thomas <robt84@gmail.com>
2023-12-31 10:31:34 -0600

    completed the why us section

--- src/app/components/global/PurpleTestimonials.js
+++ src/app/components/global/PurpleTestimonials.js
@@ -1,20 +1,26 @@
-'use client'
+"use client";
 
-import React from 'react'
+import React from "react";
 
 function PurpleTestimonials() {
   return (
-    <section className="bg-gray-900 py-24 sm:py-32">
+    <section className="bg-em-dark-purple py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
-            <img className="h-12 self-start" src="https://tailwindui.com/img/logos/tuple-logo-white.svg" alt="" />
+            <img
+              className="h-12 self-start"
+              src="https://tailwindui.com/img/logos/tuple-logo-white.svg"
+              alt=""
+            />
             <figure className="mt-10 flex flex-auto flex-col justify-between">
               <blockquote className="text-lg leading-8 text-white">
                 <p>
-                  “Amet amet eget scelerisque tellus sit neque faucibus non eleifend. Integer eu praesent at a. Ornare
-                  arcu gravida natoque erat et cursus tortor consequat at. Vulputate gravida sociis enim nullam
-                  ultricies habitant malesuada lorem ac. Tincidunt urna dui pellentesque sagittis.”
+                  “Amet amet eget scelerisque tellus sit neque faucibus non
+                  eleifend. Integer eu praesent at a. Ornare arcu gravida
+                  natoque erat et cursus tortor consequat at. Vulputate gravida
+                  sociis enim nullam ultricies habitant malesuada lorem ac.
+                  Tincidunt urna dui pellentesque sagittis.”
                 </p>
               </blockquote>
               <figcaption className="mt-10 flex items-center gap-x-6">
@@ -31,13 +37,18 @@ function PurpleTestimonials() {
             </figure>
           </div>
           <div className="flex flex-col border-t border-white/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
-            <img className="h-12 self-start" src="https://tailwindui.com/img/logos/reform-logo-white.svg" alt="" />
+            <img
+              className="h-12 self-start"
+              src="https://tailwindui.com/img/logos/reform-logo-white.svg"
+              alt=""
+            />
             <figure className="mt-10 flex flex-auto flex-col justify-between">
               <blockquote className="text-lg leading-8 text-white">
                 <p>
-                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat proident duis dolore nulla veniam
-                  reprehenderit nisi officia voluptate incididunt exercitation exercitation elit. Nostrud veniam sint
-                  dolor nisi ullamco.”
+                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
+                  proident duis dolore nulla veniam reprehenderit nisi officia
+                  voluptate incididunt exercitation exercitation elit. Nostrud
+                  veniam sint dolor nisi ullamco.”
                 </p>
               </blockquote>
               <figcaption className="mt-10 flex items-center gap-x-6">
@@ -47,7 +58,9 @@ function PurpleTestimonials() {
                   alt=""
                 />
                 <div className="text-base">
-                  <div className="font-semibold text-white">Joseph Rodriguez</div>
+                  <div className="font-semibold text-white">
+                    Joseph Rodriguez
+                  </div>
                   <div className="mt-1 text-gray-400">CEO of Reform</div>
                 </div>
               </figcaption>
@@ -56,7 +69,7 @@ function PurpleTestimonials() {
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default PurpleTestimonials
+export default PurpleTestimonials;
--- src/app/components/home/HomeWhyUs.js
+++ src/app/components/home/HomeWhyUs.js
@@ -1,82 +1,78 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import React from "react";
+import {
+  CheckBadgeIcon,
+  PuzzlePieceIcon,
+  RocketLaunchIcon,
+} from "@heroicons/react/20/solid";
 
 const features = [
-    {
-      name: 'Push to deploy.',
-      description:
-        'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-      icon: CloudArrowUpIcon,
-    },
-    {
-      name: 'SSL certificates.',
-      description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
-      icon: LockClosedIcon,
-    },
-    {
-      name: 'Database backups.',
-      description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
-      icon: ServerIcon,
-    },
-  ]
+  {
+    name: "Proven Expertise",
+    description:
+      "Decades of experience in delivering high-quality, innovative digital solutions across various industries.",
+    icon: CheckBadgeIcon,
+  },
+  {
+    name: "Client-Centric Approach",
+    description:
+      "Dedicated to understanding and meeting your needs, ensuring personalized and effective solutions.",
+    icon: PuzzlePieceIcon,
+  },
+  {
+    name: "Cutting-Edge Technology",
+    description:
+      "Continuously evolving with the latest technologies to provide advanced, future-proof solutions.",
+    icon: RocketLaunchIcon,
+  },
+];
 
 function HomeWhyUs() {
   return (
     <div className="overflow-hidden bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl md:px-6 lg:px-8">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:grid-cols-2 lg:items-start">
-          <div className="px-6 md:px-0 lg:pr-4 lg:pt-4">
-            <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-lg">
-              <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base font-semibold leading-7 text-em-pink">
+                Your Choice for Digital Excellence
+              </h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                Why Choose Empuls3: Unmatched Expertise and Innovation
+              </p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
-                iste dolor cupiditate blanditiis ratione.
+                Empuls3 stands out with its unique blend of creativity,
+                technology, and client-focused solutions, driving digital
+                success.
               </p>
               <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
                 {features.map((feature) => (
                   <div key={feature.name} className="relative pl-9">
                     <dt className="inline font-semibold text-gray-900">
-                      <feature.icon className="absolute left-1 top-1 h-5 w-5 text-indigo-600" aria-hidden="true" />
+                      <feature.icon
+                        className="absolute left-1 top-1 h-5 w-5 text-em-yellow"
+                        aria-hidden="true"
+                      />
                       {feature.name}
-                    </dt>{' '}
+                    </dt>{" "}
                     <dd className="inline">{feature.description}</dd>
                   </div>
                 ))}
               </dl>
             </div>
           </div>
-          <div className="sm:px-6 lg:px-0">
-            <div className="relative isolate overflow-hidden bg-indigo-500 px-6 pt-8 sm:mx-auto sm:max-w-2xl sm:rounded-3xl sm:pl-16 sm:pr-0 sm:pt-16 lg:mx-0 lg:max-w-none">
-              <div
-                className="absolute -inset-y-px -left-3 -z-10 w-full origin-bottom-left skew-x-[-30deg] bg-indigo-100 opacity-20 ring-1 ring-inset ring-white"
-                aria-hidden="true"
-              />
-              <div className="mx-auto max-w-2xl sm:mx-0 sm:max-w-none">
-                <div className="w-screen overflow-hidden rounded-tl-xl bg-gray-900 ring-1 ring-white/10">
-                  <div className="flex bg-gray-800/40 ring-1 ring-white/5">
-                    <div className="-mb-px flex text-sm font-medium leading-6 text-gray-400">
-                      <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
-                        NotificationSetting.jsx
-                      </div>
-                      <div className="border-r border-gray-600/10 px-4 py-2">App.jsx</div>
-                    </div>
-                  </div>
-                  <div className="px-6 pb-14 pt-6">{/* Your code example */}</div>
-                </div>
-              </div>
-              <div
-                className="pointer-events-none absolute inset-0 ring-1 ring-inset ring-black/10 sm:rounded-3xl"
-                aria-hidden="true"
-              />
-            </div>
-          </div>
+          <img
+            src="./images/Group_black_Adult_Hipsters.png"
+            alt="Group black Adult Hipsters"
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            width={2432}
+            height={1442}
+          />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default HomeWhyUs
+export default HomeWhyUs;
